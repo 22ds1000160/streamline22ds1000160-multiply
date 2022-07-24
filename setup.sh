c92d81ca3ca7dd2580e650e795435e9ca481b7a9
22ds1000160 <109895901+22ds1000160@users.noreply.github.com>
2022-07-24 16:24:39 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"22ds1000160@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
